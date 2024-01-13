bbe23494163707b834fb9115dca8aa7fd8ea1d9a
Noor Fatma <nfatma8989@gmail.com>
2024-01-13 21:10:54 +0530

    Welcome Message

---
+++ UserRegistrationSystem.java
@@ -0,0 +1,14 @@
+package com.junit.practiceproblem.UserRegistrationSytsem;
+
+
+public class UserRegistrationSystem { 
+
+	public static String welcome(String a) {
+		
+		if(a=="Calling Welcome Method")
+			return "Welcome to User Registration System ! ";
+		else
+			return "Invalid String";
+	}
+
+}
